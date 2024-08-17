0fb8082dcdf1d95c91ac633376a53097ac2063b1
Noia <edwin.lecomte31@gmail.com>
2024-08-17 12:07:20 +0200

    Adding basic routing to main and first page

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
-package watdowedo
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"watdowedo/internal/pages/home"
+)
+
+const SERVER_PORT = ":8080"
 
 func main() {
-
+	http.HandleFunc("/", home.Home)
+	fmt.Println("(http://localhost" + SERVER_PORT + ")")
+	http.ListenAndServe(SERVER_PORT, nil)
 }
