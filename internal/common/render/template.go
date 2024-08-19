8825f49f642fe22df7b39cd5100d1dc7b2acdf0a
Noia <edwin.lecomte31@gmail.com>
2024-08-19 19:30:39 +0200

    #1 feature : Adding node modules

--- internal/common/render/template.go
+++ internal/common/render/template.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RenderTemplates(w http.ResponseWriter, tmpl string) {
-	template, err := template.ParseFiles(filepath.Join("templates", tmpl+".page.tmpl"))
+	template, err := template.ParseFiles(filepath.Join("src", "templates", tmpl+".page.tmpl"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
