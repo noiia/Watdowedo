3219eaae391cd2c08a18c5636654e9db88fd11c2
Noia <edwin.lecomte31@gmail.com>
2024-08-20 00:42:12 +0200

    #1 feature : Creating real tree structure, adding init, logger and flag parsing

--- internal/common/render/template.go
+++ internal/common/render/template.go
@@ -7,9 +7,10 @@ import (
 )
 
 func RenderTemplates(w http.ResponseWriter, tmpl string) {
-	template, err := template.ParseFiles(filepath.Join("src", "templates", tmpl+".page.tmpl"))
+	template, err := template.ParseFiles(filepath.Join("../../web", "templates", tmpl+".page.tmpl"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+
 		return
 	}
 
