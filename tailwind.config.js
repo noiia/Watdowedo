21615ae3301a58eb5ef7677cafb9625897521eb8
Noia <edwin.lecomte31@gmail.com>
2024-08-19 12:19:38 +0200

    #1 feature : Adding tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+	theme: {
+		fontFamily: {
+			display: ["Inter"],
+		},
+		colors: {
+			primary: "#0094eb",
+			"primary-content": "#000813",
+			secondary: "#3d8a00",
+			"secondary-content": "#010700",
+			accent: "#005fff",
+			"accent-content": "#cfe2ff",
+			neutral: "#0f0e05",
+			"neutral-content": "#c8c8c5",
+			"base-100": "#fffdff",
+			"base-200": "#dedcde",
+			"base-300": "#bebcbe",
+			"base-content": "#161616",
+			info: "#2e93ff",
+			"info-content": "#010816",
+			success: "#008200",
+			"success-content": "#d3e6d1",
+			warning: "#df7400",
+			"warning-content": "#120500",
+			error: "#be123c",
+			"error-content": "#f43f5e",
+		},
+	},
+	plugins: [require("autoprefixer")],
+};
