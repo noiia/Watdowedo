99e1717c1905397152663b54134542c21a23d48b
Noiia <edwin.lecomte31@gmail.com>
2024-09-02 10:38:47 +0200

    #1 feature : loading css to the file server. File path error during loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{tmpl,html,js}"],
+	content: ["./web/**/*.{tmpl,html}"],
 	theme: {
 		extend: {
 			fontFamily: {
