e5046c739bd8f6d3932291960833e671f1420b6c
noiia <edwin.lecomte31@gmail.com>
2024-10-07 16:47:12 +0200

    tidy env

--- internal/common/render/template.go
+++ internal/common/render/template.go
@@ -12,14 +12,14 @@ func RenderTemplates(w http.ResponseWriter, tmpl string) {
 	template, err := template.ParseFiles(filepath.Join("../../web", "templates", tmpl+".page.tmpl"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		logger.GlobalLogger.Error(err.Error(), http.StatusInternalServerError)
+		logger.GlobalLogger.Error(err.Error() + string(http.StatusInternalServerError))
 
 		return
 	}
 	base := basetemplate.LoadBase()
 	if err := template.Execute(w, base); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		logger.GlobalLogger.Error(err.Error(), http.StatusInternalServerError)
+		logger.GlobalLogger.Error(err.Error() + string(http.StatusInternalServerError))
 
 		return
 	}
